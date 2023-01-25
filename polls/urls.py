6cd05db32a9e960364cdac10e51807a4a7d96158
Callen44 <97454255+Callen44@users.noreply.github.com>
2023-01-24 20:13:10 -0600

    fixing a bug
    this commit fixes a bug that I created when editing the html file

--- polls/urls.py
+++ polls/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    # url for new post
+    path('new/', views.new, name='new')
 ]
