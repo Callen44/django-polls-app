06a8e709c1b0ddac5afda0b95af5a757b1b49ae4
Callen44 <97454255+Callen44@users.noreply.github.com>
2023-01-24 20:21:20 -0600

    shorten views.py
    this commit shortens views.py it also includes some changes to the file that are not related to this commit.

--- polls/views.py
+++ polls/views.py
@@ -31,25 +31,6 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    template = loader.get_template('polls/index.html')
-#    context = {
-#        'latest_question_list': latest_question_list,
-#    }
-#    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -70,5 +51,8 @@ def vote(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print(request)
     return render(request, 'polls/results.html', {'question': question})
 
+def new(request):
+    pass
